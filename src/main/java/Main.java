faf1350d4f29ed09aa4c27d42e7d3afb18469fa9
tombaines <tombaines1997@gmail.com>
2020-08-06 22:58:07 +0100

    2nd commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,20 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class Main {
     public static void main(String args[]) throws IOException {
         String csvPath = "C:\\Users\\tomba\\Downloads\\788816_1355729_compressed_iot_telemetry_data.csv\\iot_telemetry_data.csv";
+
         String row = "";
         int i = 0;
-        String[] data
+
         BufferedReader csvReader = new BufferedReader(new FileReader(csvPath));
         while ((row = csvReader.readLine()) != null) {
-            i++;
-            data = row.split(",");
+            String[] data = row.split(",");
+            System.out.println("TS: " + data[0] + " Temp: " + data[8]);
         }
-        
-        System.out.println(data);
-
-
     }
-
 }
